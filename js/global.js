411d1d9ce55baba8ec27817fe9b7e5441f695984
Andry Sobolev <allmywebwork@gmail.com>
2016-03-24 19:45:16 +0200

    Add 'posted in' text to blog term

--- js/global.js
+++ js/global.js
@@ -13,15 +13,11 @@
       });
 
       /**
-       * jQuery page scroll Speed
+       * Add 'posted in' text to blog term
        */
-      //https://github.com/nathco/jQuery.scrollSpeed
-      // jQuery.scrollSpeed(50, 1400);
-
-      /**
-       * Parallax
-       */
-      //$('#').parallax({imageSrc: '/sites/all/themes/.../.jpg'});
+      var term = $('.node-type-blog .field-name-field-term .field-item').html();
+      $('.node-type-blog .field-name-field-term .field-item a').remove()
+      $('.node-type-blog .field-name-field-term .field-item').append('(posted in ' + (term) + ')');
 
       /**
        * back to Top
@@ -33,17 +29,6 @@
         'speed' : 500,
         'color' : '#cccccc',
       });
-
-      /**
-       * Add mobile event support for bootstrap slider
-       * http://lazcreative.com/blog/adding-swipe-support-to-bootstrap-carousel-3-0/
-       */
-      //$("#views-bootstrap-carousel-1").swiperight(function() {
-      //  $(this).carousel('prev');
-      //});
-      //$("#views-bootstrap-carousel-1").swipeleft(function() {
-      //  $(this).carousel('next');
-      //});
     }
   };
 }(jQuery));
